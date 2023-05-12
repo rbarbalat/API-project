b8924f121d3ecfef331cca3fa7794e35a05a9c3b
Roman Barbalat <roman.barbalat22@gmail.com>
2023-05-11 21:54:28 -0400

    Added user input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -5,8 +5,32 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
+/*
+The validateSignup middleware is composed of the check and handleValidationErrors middleware. It checks to see if req.body.email exists and is an email, req.body.username is a minimum length of 4 and is not an email, and req.body.password is not empty and has a minimum length of 6. If at least one of the req.body values fail the check, an error will be returned as the response.
+*/
+
+const validateSignup =
+[
+    check('email').exists({ checkFalsy: true }).isEmail()
+    .withMessage('Please provide a valid email.'),
+
+    check('username').exists({ checkFalsy: true }).isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+
+    check('username').not().isEmail()
+    .withMessage('Username cannot be an email.'),
+
+    check('password').exists({ checkFalsy: true })
+    .isLength({ min: 6 }).withMessage('Password must be 6 characters or more.'),
+
+    handleValidationErrors
+];
+
 //signup
-router.post('/', async (req, res) => {
+router.post('/', validateSignup, async (req, res) => {
     const { email, password, username } = req.body;
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({ email, username, hashedPassword });
