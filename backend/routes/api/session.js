bc5dc5193fd247fa8b3c25e830c109d61970d3e0
Roman Barbalat <roman.barbalat22@gmail.com>
2023-05-11 22:46:48 -0400

    Adjusted route handlers to account for firstName and lastName columns

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -51,6 +51,8 @@ router.post('/', validateLogin, async (req, res, next) => {
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
 
       await setTokenCookie(res, safeUser);
@@ -77,6 +79,8 @@ router.get('/', (req, res) => {
         id: user.id,
         email: user.email,
         username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName
       };
       return res.json({
         user: safeUser
