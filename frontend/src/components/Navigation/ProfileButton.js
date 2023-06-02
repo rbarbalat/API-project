6b24572640a3f8a151dca4fe7f2c5587f2b31c61
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-02 18:42:05 -0400

    finished authMe part 3

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -1,31 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({ user }) {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
+
+  const openMenu = () => {
+    if (showMenu) return;
+    setShowMenu(true);
+  };
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    document.addEventListener('click', closeMenu);
+
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
   const logout = (e) => {
     e.preventDefault();
     dispatch(sessionActions.logout());
   };
 
-  const ulClassName = "profile-dropdown";
+  // added .hidden { display: none} to index.css (site wide styles)
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
 
   return (
     <>
-      <button>
+      <button onClick={openMenu}>
         <i className="fa-solid fa-user" />
-        {/* <i className="fas fa-user-circle" /> */}
       </button>
-      <ul className="profile-dropdown">
+      <ul className={ulClassName} ref={ulRef}>
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
-        {/* extra button? there is already one inside the li that contains ProfileButton */}
-        {/* <li>
+        <li>
           <button onClick={logout}>Log Out</button>
-        </li> */}
+        </li>
       </ul>
     </>
   );
