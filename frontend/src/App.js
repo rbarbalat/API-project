3eecb66cb76e039c91f0ed1b4d9fe09f9132815b
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-05 18:49:00 -0400

    added a link to all groups and a signup button to the landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import * as sessionActions from "./store/session";
 import Navigation from "./components/Navigation";
 import AllGroups from "./components/AllGroups";
 import SingleGroup from "./components/SingleGroup";
+import LandingPage from "./components/LandingPage";
 
 //current
 function App() {
@@ -19,11 +20,12 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded &&
         <Switch>
+            <Route exact path="/">
+              <LandingPage />
+            </Route>
             <Route exact path="/groups">
               <AllGroups />
             </Route>
-            {/* maybe :groupId should be only reachable by Link from /groups not directly
-            remove from Route? */}
             <Route path="/groups/:groupId">
               <SingleGroup />
             </Route>
