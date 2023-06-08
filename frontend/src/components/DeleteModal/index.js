31e5ab28db1f656c4e0184a43a8efeee7d758362
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-07 22:27:56 -0400

    created a delete modal for deleting groups and events

---
+++ frontend/src/components/DeleteModal/index.js
@@ -0,0 +1,38 @@
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useModal } from "../../context/Modal";
+import "./DeleteModal.css";
+import { thunkDeleteGroup } from "../../store/groups";
+
+export default function DeleteModal({typeId, type})
+{
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const { closeModal } = useModal();
+    async function onDeleteClick()
+    {
+        //test for userIsOrganizer will take place inside the group/groupId page
+        if(type === "group")
+        {
+            const serverObject = await dispatch(thunkDeleteGroup(typeId));
+            if(serverObject.message === "Successfully deleted")
+            {
+                console.log("the group was deleted");
+                history.replace("/groups");
+                closeModal();
+            }else{
+                //adjust later
+                return window.alert("Something went wrong");
+            }
+        }
+    }
+
+    return (
+        <>
+            <div>Confirm Delete</div>
+            <div>Are you sure you want to remove this {type}?</div>
+            <button onClick={onDeleteClick}>Yes (Delete {type})</button>
+            <button onClick={closeModal}>No (Keep {type})</button>
+        </>
+    )
+}
