51c5a87ac8f7d7127d637eb8666ffda9b859e55e
Roman Barbalat <roman.barbalat22@gmail.com>
2023-06-11 13:57:51 -0400

    created a basic structure for the landing page

--- frontend/src/components/LandingPage/index.js
+++ frontend/src/components/LandingPage/index.js
@@ -2,6 +2,7 @@ import {NavLink} from "react-router-dom";
 import { useSelector } from "react-redux";
 import OpenModalButton from "../OpenModalButton";
 import SignupFormModal from "../SignupFormModal";
+import "./LandingPage.css";
 
 export default function LandingPage()
 {
@@ -13,14 +14,81 @@ export default function LandingPage()
         </div>
                         );
     return (
-        <>
-            <div>LANDING PAGE!</div>
-            <NavLink exact to ="/groups">See all groups</NavLink>
-            <div>some space</div>
-            <NavLink exact to ="/events">See all events</NavLink>
-            <div>some space</div>
-            <NavLink exact to ="/groups/new">Create Group Test</NavLink>
-            {!sessionUser && signUpButton}
-        </>
+            <div className="landingWrapper">
+
+                <div className="sectionOne">
+                    <div className="titleIntroText">
+                        {/* maybe 3 divs here for 3 lines? */}
+                        <div className="landingTitle">
+                            The animal platform-- Where interests
+                            become friendships
+                        </div>
+                        <div className="landingIntroText">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat
+                        </div>
+                    </div>
+                    <div className="infographicWrapper">
+                        <img className="infographic" src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg"alt="infographic"></img>
+                    </div>
+                </div>
+
+                <div className="sectionTwo">
+                    <div className="landingSubtitle">How meetup works</div>
+                    <div className="landingCaption">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                    </div>
+                </div>
+
+                <div className="sectionThree">
+
+                    <div className="sectionThreePart">
+                        <div className="sectionThreeImage">
+                            <img alt="alt" src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg"></img>
+                        </div>
+                        <div className="sectionThreeLink">
+                            <NavLink className="nL" exact to="/groups">See all groups</NavLink>
+                        </div>
+                        <div>
+                            <div className="sectionThreeText">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="sectionThreePart">
+                        <div className="sectionThreeImage">
+                            <img alt="alt" src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg"></img>
+                        </div>
+                        <div className="sectionThreeLink">
+                            <NavLink className="nL" exact to="/events">Find an event</NavLink>
+                        </div>
+                        <div>
+                            <div className="sectionThreeText">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="sectionThreePart">
+                        <div className="sectionThreeImage">
+                            <img alt="alt" src="https://upload.wikimedia.org/wikipedia/commons/3/3f/Walking_tiger_female.jpg"></img>
+                        </div>
+                        <div className="sectionThreeLink">
+                            <NavLink className="nL" exact to="/groups/new">Start a new group</NavLink>
+                        </div>
+                        <div>
+                            <div className="sectionThreeText">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, se
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="sectionFour">
+                    {!sessionUser && signUpButton}
+                </div>
+
+            </div>
     )
 }
