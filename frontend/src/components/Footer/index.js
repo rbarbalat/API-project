eb86f3097287a662cc7b894d65eb4d9c789a1bd0
Roman Barbalat <roman.barbalat22@gmail.com>
2023-08-12 13:35:49 -0400

    width and footer adjustments

--- frontend/src/components/Footer/index.js
+++ frontend/src/components/Footer/index.js
@@ -2,6 +2,16 @@ import "./Footer.css"
 export default function Footer()
 {
     return (
-        <div className="footer"></div>
+        <div className="footer">
+            <div className="footer_content_wrapper">
+                <span>Roman Barbalat</span>
+                <a href="https://github.com/rbarbalat" target="_blank">
+                    <i className="fa-brands fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/roman-barbalat-00140a63/" target="_blank">
+                    <i className="fa-brands fa-linkedin"></i>
+                </a>
+            </div>
+        </div>
     );
 }
